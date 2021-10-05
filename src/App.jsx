44119dc1a6d639d1024da5369f5c705dc99e24aa
franco14lorenzo <franco14lorenzo@gmail.com>
2021-10-05 16:06:07 -0300

    composed app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Display from './components/Display/Display';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>PIXEL PAINT APP</h1>
+      <Display />
     </div>
   );
 }
