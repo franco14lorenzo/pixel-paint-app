f15516cce9f115f2e6d838704b10aa9461690b15
franco14lorenzo <franco14lorenzo@gmail.com>
2021-10-05 20:26:48 -0300

    added paint function

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -1,21 +1,20 @@
 
 import './Display.css';
-
+import { useState } from 'react';
 import useWindowSize from '../../hooks/useWindowsSize.js';
 import Pixel from '../Pixel/Pixel';
-
-
 
 const Display = () => {
 
   const area = useWindowSize(); 
-  const cells = Array.from({ length: area})
+  const cells = Array.from({ length: area});
+  const [holdingClick, setHoldingClick] = useState(false);
   
   return (
-    <div id="display" >
-      {cells.map((cell, index) => <Pixel key={index}/>)}
+    <div id='display'  onMouseDown={() => setHoldingClick(true)} onMouseUp={()=> setHoldingClick(false)}>
+      {cells.map((cell, index) => <Pixel key={index} holdingClick={holdingClick}/>)}
     </div>
-  ) 
-}
+  ); 
+};
 
 export default Display;
--- src/components/Pixel/Pixel.jsx
+++ src/components/Pixel/Pixel.jsx
@@ -1,10 +1,22 @@
-import './Pixel.css'
+import './Pixel.css';
 import { useState } from 'react';
 
-const Pixel = () => {
-    const [painted, setPainted] = useState(false);
-    
-    return <div id="pixel"></div>
-}
+const Pixel = (props) => {
+  
+  const {holdingClick} = props;
+  const [painted, setPainted] = useState(false);
+  const stylePixel = {
+    background : '#000000'
+  };
+  
+  return (
+    <div 
+      id="pixel" 
+      onClick={() => setPainted(!painted)}
+      onMouseMove={(() => holdingClick ? setPainted(true) : null)} 
+      style={painted ? stylePixel : null}
+    />
+  );
+};
 
 export default Pixel;
